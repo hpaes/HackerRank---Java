c119ed2a5ae7735282239bd5d3f4eb6640e247aa
Herbert Paes <8264908+hpaes@users.noreply.github.com>
2020-05-29 02:01:05 -0300

    exercises

--- Exercises/Person.java
+++ Exercises/Person.java
@@ -2,19 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Person {
-    private int age;	
-  
+
+	private int age;
+
 	public Person(int initialAge) {
-  		// Add some more code to run some checks on initialAge
+		// Add some more code to run some checks on initialAge
+		if (initialAge < 0) {
+			age = 0;
+			System.out.println("Age is not valid, setting age to 0.");
+		} else {
+			age = initialAge;
+		}
 	}
 
 	public void amIOld() {
-  		// Write code determining if this person's age is old and print the correct statement:
-        System.out.println(/*Insert correct print statement here*/);
+		// Write code determining if this person's age is old and print the correct
+		// statement:
+		if (age < 13) {
+			System.out.println("You are young.");
+		} else if (age >= 13 && age < 18) {
+			System.out.println("You are a teenager.");
+		} else {
+			System.out.println("You are old.");
+		}
 	}
 
 	public void yearPasses() {
-  		// Increment this person's age.
+		// Increment this person's age.
+		age++;
 	}
 
 	public static void main(String[] args) {
@@ -29,7 +44,7 @@ public class Person {
 			}
 			p.amIOld();
 			System.out.println();
-        }
+		}
 		sc.close();
-    }
+	}
 }
